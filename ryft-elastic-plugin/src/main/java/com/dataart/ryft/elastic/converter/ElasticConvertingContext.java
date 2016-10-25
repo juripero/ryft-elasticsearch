ffc39db1f5a5c0535eac3d7ef026c8d46ff28c11
imasternoy <imasternoy@gmail.com>
2016-10-25 12:24:03 +0300

    Injected converters into Converting context via GUICE factory

--- ryft-elastic-plugin/src/main/java/com/dataart/ryft/elastic/converter/ElasticConvertingContext.java
+++ ryft-elastic-plugin/src/main/java/com/dataart/ryft/elastic/converter/ElasticConvertingContext.java
@@ -8,7 +8,14 @@ import com.dataart.ryft.elastic.converter.ryftdsl.RyftInputSpecifierRecord;
 import com.dataart.ryft.elastic.converter.ryftdsl.RyftOperator;
 import com.dataart.ryft.elastic.converter.ryftdsl.RyftQuery;
 import com.dataart.ryft.elastic.converter.ryftdsl.RyftQuerySimple;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
 import java.util.Map;
+import java.util.Set;
+
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.xcontent.XContentParser;
 
 public class ElasticConvertingContext {
@@ -16,16 +23,23 @@ public class ElasticConvertingContext {
     private final XContentParser contentParser;
     private final Map<String, ElasticConvertingElement> elasticConverters;
 
-    //TODO: [dendec] add default values
+    // TODO: [dendec] add default values
     private RyftFuzzyMetric metric = null;
     private Integer fuzziness = null;
     private String searchText = null;
     private String fieldName = null;
+    private String originalQuery = null;
 
-    public ElasticConvertingContext(XContentParser parser, 
-            Map<String, ElasticConvertingElement> converters) {
+    @Inject
+    public ElasticConvertingContext(@Assisted XContentParser parser, Set<ElasticConvertingElement> injectedConverters) {
+        Map<String, ElasticConvertingElement> convertersMap = Maps.newHashMap();
+        for (ElasticConvertingElement converter : injectedConverters) {
+            for (String name : converter.names()) {
+                convertersMap.put(name, converter);
+            }
+        }
+        this.elasticConverters = ImmutableMap.copyOf(convertersMap);
         this.contentParser = parser;
-        this.elasticConverters = converters;
     }
 
     public XContentParser getContentParser() {
@@ -39,7 +53,7 @@ public class ElasticConvertingContext {
         }
         return result;
     }
-    
+
     public RyftFuzzyMetric getMetric() {
         return metric;
     }
@@ -71,25 +85,31 @@ public class ElasticConvertingContext {
     public void setFieldName(String fieldName) {
         this.fieldName = fieldName;
     }
-    
+
+    public void setOriginalQuery(String originalQuery) {
+        this.originalQuery = originalQuery;
+    }
+
+    public String getOriginalQuery() {
+        return originalQuery;
+    }
+
     public void clean() {
         fieldName = null;
         searchText = null;
         fuzziness = null;
-        metric = null;        
+        metric = null;
     }
 
     RyftQuery constructFuzzyQuery() {
-        if ((fieldName != null) && (searchText != null)
-                && (fuzziness != null) && (metric != null)) {
+        if ((fieldName != null) && (searchText != null) && (fuzziness != null) && (metric != null)) {
             RyftExpression ryftExpression;
             if (fuzziness == 0) {
                 ryftExpression = new RyftExpressionExactSearch(searchText);
             } else {
                 ryftExpression = new RyftExpressionFuzzySearch(searchText, metric, fuzziness);
             }
-            return new RyftQuerySimple(new RyftInputSpecifierRecord(fieldName),
-                    RyftOperator.CONTAINS, ryftExpression);
+            return new RyftQuerySimple(new RyftInputSpecifierRecord(fieldName), RyftOperator.CONTAINS, ryftExpression);
         } else {
             return null;
         }
