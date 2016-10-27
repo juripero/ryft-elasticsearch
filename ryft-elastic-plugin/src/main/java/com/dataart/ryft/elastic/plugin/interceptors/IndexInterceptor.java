282318679ba5aa1d74dec06ba286d6ada3daf0b0
imasternoy <imasternoy@gmail.com>
2016-10-27 16:23:59 +0300

    Added Global properties index called ryftpluginproperties; To enable es ryft integration add boolean field "ryft_integration_enabled":true to the index ryftpluginproperties with id=1

--- ryft-elastic-plugin/src/main/java/com/dataart/ryft/elastic/plugin/interceptors/IndexInterceptor.java
+++ ryft-elastic-plugin/src/main/java/com/dataart/ryft/elastic/plugin/interceptors/IndexInterceptor.java
@@ -4,28 +4,39 @@ import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.support.ActionFilterChain;
+import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.tasks.Task;
 
+import com.dataart.ryft.elastic.plugin.PropertiesProvider;
+import com.dataart.ryft.elastic.plugin.RyftPluginGlobalSettingsProvider;
+
 public class IndexInterceptor implements ActionInterceptor {
     private final ESLogger logger = Loggers.getLogger(getClass());
+    PropertiesProvider propertiesProvider;
+
+    @Inject
+    public IndexInterceptor(PropertiesProvider provider) {
+        propertiesProvider = provider;
+    }
 
     @Override
     public boolean intercept(Task task, String action, ActionRequest request, ActionListener listener,
             ActionFilterChain chain) {
+        String settingsIndex = propertiesProvider.get().getStr(PropertiesProvider.PLUGIN_SETTINGS_INDEX);
         if (request instanceof IndexRequest) {
             IndexRequest req = (IndexRequest) request;
             String id = req.id(); // Document id
             String type = req.type();
             String index = req.index();
-            String source = new String(req.source().array());// Document source
-            logger.info("Write request received: id: {}, index/type: {}/{}, source: {}", id, index,type,source);
-
+            if (settingsIndex.equals(index)) {
+              return true;
+            }
         } else {
             logger.info("Intercepted request: {}", request);
         }
-        return true;
+        return false;
     }
 
 }
